6111c1c1eca06b1ad792a86d8ed1410089930f97
Jay Sun Vijan <jvijan@umich.edu>
2026-01-15 20:19:03 -0500

    Made theo

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,11 @@
 extends Actor
 
 @export var speed: float = 10000000
+@export var fireSpeed: float = 0.25
+@export var actorScene: PackedScene
+@export var shootSpawn: Vector2
+
+var lastFired: float = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,6 +14,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	lastFired -= delta
 	if Input.is_action_pressed("playerUp"):
 		position.y -= (speed * delta)
 	elif Input.is_action_pressed("playerDown"):
@@ -17,3 +23,13 @@ func _process(delta: float) -> void:
 		position.x += (speed * delta)
 	elif Input.is_action_pressed("playerLeft"):
 		position.x -= (speed * delta)
+	if Input.is_action_just_pressed("fireTheo"):
+		if lastFired <= 0:
+			# Spawn theo and shoot
+			spawnActor()
+			lastFired = fireSpeed
+			
+func spawnActor():
+	var newActor = actorScene.instantiate()
+	newActor.global_position = global_position + shootSpawn
+	get_parent().add_child(newActor)
